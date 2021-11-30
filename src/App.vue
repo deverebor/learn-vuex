814a04e2bafc2e711a31772c247d83ad24dfe81e
Lucas Souza <lucasp.sdev@gmail.com>
2021-11-30 09:47:20 -0300

    feat: form example (#1)

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,55 @@
 <template>
-  {{ $store.state.user.firstName }}
-  <br />
-  <input v-model="myName" type="text" />
+  <div class="container mt-8">
+    <span class="user-name">
+      {{ $store.state.user.firstName }} {{ $store.state.user.lastName }}
+    </span>
+    <br />
+    <label id="top-label" for="">Primeiro Nome</label>
+    <br />
+    <input class="form-control" v-model="changeFirstName" type="text" />
+    <br />
+    <label for="">Sobre Nome</label>
+    <br />
+    <input class="form-control" v-model="changeLastName" type="text" />
 
-  <button @click.prevent.stop="saveName">Save</button>
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button class="btn btn-primary" @click.prevent.stop="saveName">Save</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 import { App } from "../src/utils/interfaces/App";
 
 export default defineComponent<App>({
   name: "App",
-  components: {
-    HelloWorld,
-  },
   data() {
     return {
       myName: "",
     };
   },
+  computed: {
+    changeFirstName: {
+      get() {
+        return this.$store.state.user.firstName;
+      },
+      set(value: string) {
+        this.$store.commit("saveFirstName", value);
+      },
+    } as any,
+
+    changeLastName: {
+      get() {
+        return this.$store.state.user.lastName;
+      },
+      set(value: string) {
+        this.$store.commit("saveLastName", value);
+      },
+    } as any,
+  },
   methods: {
-    saveName(){
+    saveName() {
       //Action
-      this.$store.dispatch('saveFirstName', this.myName);
+      this.$store.dispatch("saveFirstName", this.myName);
 
       //Mutation
       //this.$store.commit('saveFirstName', this.myName);
@@ -34,13 +58,4 @@ export default defineComponent<App>({
 });
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style src="./App.scss" lang="scss" />
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
 import { createStore } from "vuex";
 
 export default createStore({
+  strict: true,
   state: {
     user: {
       firstName: "Jhon",
@@ -12,10 +13,16 @@ export default createStore({
     saveFirstName(state, payload) {
       state.user.firstName = payload;
     },
+    saveLastName(state, payload) {
+      state.user.lastName = payload;
+    },
   },
   actions: {
     saveFirstName(context, payload) {
       context.commit("saveFirstName", payload);
+    },
+    saveLastName(context, payload) {
+      context.commit("saveLastName", payload);
     },
   },
   modules: {},
